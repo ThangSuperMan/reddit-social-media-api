032d9716aee867cb8e01b73c9dae4121832f48d8
thangsuperman <thangjenny2002@gmail.com>
2024-01-27 22:40:46 +0700

    Refactor S3 upload file functionality

--- lib/s3.rb
+++ lib/s3.rb
@@ -4,11 +4,14 @@ module S3
   def s3_upload(file)
     s3_bucket = Aws::S3::Resource.new.bucket(ENV['AWS_S3_BUCKET'])
 
+    upload_file(s3_bucket, file)
+  end
+
+  def upload_file(s3_bucket, file)
     file_name = generate_file_name(file)
     object = s3_bucket.object(file_name)
     file_temp_location_path = file.tempfile.path
     object.upload_file(file_temp_location_path)
-
     file_name
   end
 
