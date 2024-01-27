2d14ca433c6a87eaac68e8fd4fd3a5d4b9d12a0f
thangsuperman <thangjenny2002@gmail.com>
2024-01-27 21:40:24 +0700

    Refactor json response for create user api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -16,8 +16,9 @@ module Api
             expires_in: Doorkeeper.configuration.access_token_expires_in
           )
 
-          render json: {
-            user: {
+          json_response(
+            message: 'Created user successfully',
+            metadata: {
               id: user.id,
               email: user.email,
               access_token: access_token.token,
@@ -25,8 +26,9 @@ module Api
               expires_in: access_token.expires_in,
               refresh_token: access_token.refresh_token,
               created_at: access_token.created_at.to_time.to_i
-            }
-          }, status: :created
+            },
+            status: :created
+          )
         else
           render json: { error: user.errors.full_messages }, status: :unprocessable_entity
         end
